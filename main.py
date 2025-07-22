365d1fd8dbce490cc42fde8d4150fb2e10dd149e
Brendan <bmkersey@gmail.com>
2025-07-22 12:53:32 -0400

    added initial AI setup

--- main.py
+++ main.py
@@ -1,6 +1,51 @@
-def main():
-    print("Hello from py-agent!")
+import os
+import sys
+from google import genai
+from dotenv import load_dotenv
+from google.genai import types
 
+
+def main():
+    load_dotenv()
+    verbose = "--verbose" in sys.argv
+    args = []
+    for arg in sys.argv[1:]:
+        if not arg.startswith("--"):
+            args.append(arg)
+
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here" [--verbose]')
+        print('Example: python main.py "How do I build a calculator app?"')
+        sys.exit(1)
+
+    
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    user_prompt = sys.argv[1]
+    
+    user_prompt = " ".join(args)
+    if verbose:
+        print("User prompt:", user_prompt)
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)])
+    ]
+
+    generate_content(client, messages, verbose)
+
+
+def generate_content(client, messages, verbose):
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages,
+    )
+
+    if verbose:
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
+    print("Response:")
+    print(response.text)
 
 if __name__ == "__main__":
     main()
